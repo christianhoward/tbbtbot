ef746d67cb8f7f646eb82e6a4018882fbfc1b5da
christianhoward <christian.john.howard@gmail.com>
2016-12-11 18:36:40 -0500

    Updates to README, reply_post, and .gitignore.

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ A reddit bot built using Python3 and the Python Reddit API Wrapper package.
 - Python3
 - Python Reddit API Wrapper (PRAW)
 
+## Project Description
+This project was to create a RedditBot that monitors posts in a subreddit that I am a mod for, [/r/bigbangtheory](http://www.reddit.com/r/bigbangtheory). The bot monitors posts for various keywords in the submission title that would suggest that the post contains spoilers, and will reply if one of the words is found. The goal is to cut down on new episodes being ruined for viewers.
+
+## Decisions
+For the initial build, I used an if conditional to test for theory keywords that I believed were prevalent in the spoiler posts I've seen on the forum: "[spoilers]", "promo", and "photos".
+
 ## Questions??
 If you have any questions, please feel free to contact me.
--- reply_post.py
+++ reply_post.py
@@ -22,21 +22,15 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
+searchWords = ["photos", "promo", "[spoilers]"]
 subreddit = r.get_subreddit("bigbangtheory")
 for submission in subreddit.get_hot(limit=5):
     if submission.id not in posts_replied_to:
-        if re.search("promo", submission.title, re.IGNORECASE):
-            submission.add_comment('I noticed your post contains content that could be considered a "spoiler". Please add the Spoiler flair to your post!')
-            print("Bot replying to : ", submission.title)
-            posts_replied_to.append(submission.id)
-        elif re.search("photos", submission.title, re.IGNORECASE):
-            submission.add_comment('I noticed your post contains content that could be considered a "spoiler". Please add the Spoiler flair to your post!')
-            print("Bot replying to : ", submission.title)
-            posts_replied_to.append(submission.id)
-        elif re.search("[spoilers]", submission.title, re.IGNORECASE):
-            submission.add_comment('I noticed your post contains content that could be considered a "spoiler". Please add the Spoiler flair to your post!')
-            print("Bot replying to : ", submission.title)
-            posts_replied_to.append(submission.id)
+        for searchWord in searchWords:
+            if re.search(searchWord, submission.title, re.IGNORECASE):
+                submission.add_comment('I noticed your post contains content that could be considered a "spoiler". Please add the Spoiler flair to your post!')
+                print("Bot replying to : ", submission.title)
+                posts_replied_to.append(submission.id)
                         
 with open("posts_replied_to.txt", "w") as f:
     for post_id in posts_replied_to:
